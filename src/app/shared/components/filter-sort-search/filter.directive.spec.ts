3960fe2adaf14bc648f474a3cf30460bc2255e4c
Nemanja Kontic <konticnemanja1@gmail.com>
2021-09-04 16:36:24 +0200

    eslint and stylelint and everything cleaned

--- src/app/shared/components/filter-sort-search/filter.directive.spec.ts
+++ src/app/shared/components/filter-sort-search/filter.directive.spec.ts
@@ -1,8 +1,13 @@
+import { ElementRef, Renderer2, ViewContainerRef } from '@angular/core';
+import { inject } from '@angular/core/testing';
 import { FilterDirective } from './filter.directive';
 
 describe('FilterDirective', () => {
-  it('should create an instance', () => {
-    const directive = new FilterDirective();
-    expect(directive).toBeTruthy();
-  });
+  it('should create an instance', inject(
+    [ElementRef, Renderer2, ViewContainerRef],
+    (el: ElementRef, renderer: Renderer2, ref: ViewContainerRef) => {
+      const directive = new FilterDirective(renderer, el, ref);
+      expect(directive).toBeTruthy();
+    }
+  ));
 });
--- src/app/shared/components/tooltip/tooltip.directive.spec.ts
+++ src/app/shared/components/tooltip/tooltip.directive.spec.ts
@@ -1,8 +1,13 @@
+import { ElementRef, Renderer2, ViewContainerRef } from '@angular/core';
+import { inject } from '@angular/core/testing';
 import { TooltipDirective } from './tooltip.directive';
 
 describe('TooltipDirective', () => {
-  it('should create an instance', () => {
-    const directive = new TooltipDirective();
-    expect(directive).toBeTruthy();
-  });
+  it('should create an instance', inject(
+    [ElementRef, Renderer2, ViewContainerRef],
+    (el: ElementRef, renderer: Renderer2, ref: ViewContainerRef) => {
+      const directive = new TooltipDirective(renderer, el, ref);
+      expect(directive).toBeTruthy();
+    }
+  ));
 });
